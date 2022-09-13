a2f7ca82f5ef46a113204e58677fac66d981043d
mu2ku <gmutuku@gmail.com>
2022-09-13 19:04:02 -0400

    commit database set-up

---
+++ config/database.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+const db = mongoose.connection
+
+mongoose.connect(process.env.DATABASE_URL)
+.then(()=>{})
+.catch((error)=>{
+  console.log(error)
+})
+
+// database connection event
+db.on('connected', function () {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`)
+})
---
+++ routes/characters.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+
+export {router}
