13d6065647b5e4e32260cd8c2127d0073b8a8305
mu2ku <gmutuku@gmail.com>
2022-09-16 15:47:46 -0400

    commit cleaned up signup controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,18 @@
 import { User } from '../models/user.js'
+import { Profile } from '../models/profile.js'
+import jwt from 'jsonwebtoken'
 
-//handle errors
+// Handle errors
 function handleErrors(err){
   let errors = {email:"",password:""}
 
-  //duplicate error code
+  //duplicate user error code
   if(err.code === 11000){
     errors.email = 'That email is already registered'
     return errors
   }
 
-  //validation errors
+  //email or password validation errors
   if(err.message.includes('User validation failed')){
     Object.values(err.errors).forEach(({properties}) =>{
       errors[properties.path] = properties.message
@@ -19,20 +21,22 @@ function handleErrors(err){
   return errors
 }
 
-function signup_get(req,res){
-  res.render('signup')
+// Create JWT 
+function createJWT(user){
+  return jwt.sign({user}, process.env.SECRET, {expiresIn: '24h'})
 }
 
-function login_get(req,res){
-  res.render('login')
-}
-
-async function signup_post(req,res){
-  const {email, password} = req.body
+async function signup(req,res){
+  const profile = new Profile(req.body)
+  const user = new User(req.body)
+  req.body.profile = profile._id
   try {
-    const user = await User.create({ email, password})
-    res.status(201).json(user)
+    await user.save()
+    await profile.save()
 
+    const token = createJWT(user._id)
+    res.cookie('jwt', token, {httpOnly: true, maxAge: 3*24*60*60*1000})
+    res.status(201).json({user: user._id})
   }
   catch(err){
     const errors = handleErrors(err)
@@ -40,13 +44,11 @@ async function signup_post(req,res){
   }
 }
 
-async function login_post(req,res){
+async function login(req,res){
   res.send('user login')
 }
 
 export{
-  signup_get,
-  signup_post,
-  login_get,
-  login_post
+  signup,
+  login
 }
